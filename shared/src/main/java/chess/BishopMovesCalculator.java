24dfd0af405a766c45e1ab4f22114fe188ec7a28
Levi Draughon <ldraugho@byu.edu>
2026-01-21 18:50:36 -0700

    Passed bishop move tests!!

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,12 +1,11 @@
 package chess;
 
 import java.util.Collection;
-import java.util.List;
+
 
 public class BishopMovesCalculator extends PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ChessPiece piece = board.getPiece(myPosition);
-        return List.of();
+        return this.diagonalMoves(board, myPosition);
     }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class PieceMovesCalculator {
 
@@ -18,11 +20,67 @@ public class PieceMovesCalculator {
         boolean q4 = true;
         int rowShift = 1;
         int colShift = 1;
-        while (rowShift < 8 && colShift < 8){
-
+        List<ChessPosition> validSquares = new ArrayList<>();
+        while (rowShift < 7 && colShift < 7){
+            if (q1){
+                ChessPosition next = new ChessPosition(currentRow-rowShift, currentCol-colShift);
+                if (next.getRow() < 1 || next.getColumn() < 1) {
+                    q1 = false;
+                } else if (board.getPiece(next) == null) {
+                    validSquares.add(next);
+                } else if (board.getPiece(next).getTeamColor() == piece.getTeamColor()) {
+                    q1 = false;
+                } else if (board.getPiece(next).getTeamColor() != piece.getTeamColor()) {
+                    validSquares.add(next);
+                    q1 = false;
+                }
+            }
+            if (q2){
+                ChessPosition next = new ChessPosition(currentRow-rowShift, currentCol+colShift);
+                if (next.getRow() < 1 || next.getColumn() > 8) {
+                    q2 = false;
+                } else if (board.getPiece(next) == null) {
+                    validSquares.add(next);
+                } else if (board.getPiece(next).getTeamColor() == piece.getTeamColor()) {
+                    q2 = false;
+                } else if (board.getPiece(next).getTeamColor() != piece.getTeamColor()) {
+                    validSquares.add(next);
+                    q2 = false;
+                }
+            }
+            if (q3){
+                ChessPosition next = new ChessPosition(currentRow+rowShift, currentCol-colShift);
+                if (next.getRow() > 8 || next.getColumn() < 1) {
+                    q3 = false;
+                } else if (board.getPiece(next) == null) {
+                    validSquares.add(next);
+                } else if (board.getPiece(next).getTeamColor() == piece.getTeamColor()) {
+                    q3 = false;
+                } else if (board.getPiece(next).getTeamColor() != piece.getTeamColor()) {
+                    validSquares.add(next);
+                    q3 = false;
+                }
+            }
+            if (q4){
+                ChessPosition next = new ChessPosition(currentRow+rowShift, currentCol+colShift);
+                if (next.getRow() > 8 || next.getColumn() > 8) {
+                    q4 = false;
+                } else if (board.getPiece(next) == null) {
+                    validSquares.add(next);
+                } else if (board.getPiece(next).getTeamColor() == piece.getTeamColor()) {
+                    q4 = false;
+                } else if (board.getPiece(next).getTeamColor() != piece.getTeamColor()) {
+                    validSquares.add(next);
+                    q4 = false;
+                }
+            }
             rowShift ++;
             colShift ++;
         }
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (ChessPosition position : validSquares){
+            moves.add(new ChessMove(myPosition, position, null));
+        }
+        return moves;
     }
 }
