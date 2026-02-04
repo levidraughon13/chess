2fb8da15e92b4a1ec93796a7d341b5479a99fe9e
Levi Draughon <ldraugho@byu.edu>
2026-02-04 14:39:01 -0700

    fixed a bug in validMoves. passing 22/43 tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -17,9 +18,9 @@ public class ChessGame {
 
     public ChessGame() {
         this.board = new ChessBoard();
+        board.resetBoard();
         setTeamTurn(TeamColor.WHITE);
         this.teamTurn = getTeamTurn();
-        setBoard(board);
     }
 
     /**
@@ -55,17 +56,17 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         Collection<ChessMove> moves = board.getPiece(startPosition).pieceMoves(this.board,startPosition);
+        setTeamTurn(board.getPiece(startPosition).getTeamColor());
         try{
             ChessBoard copy = board.clone();
-            if (isInCheck(getTeamTurn())) {
-                for (ChessMove move : moves) {
-                    board.addPiece(move.getEndPosition(), copy.getPiece(startPosition));
-                    board.addPiece(startPosition, null);
-                    if (isInCheck(getTeamTurn())) {
-                        moves.remove(move);
-                    }
-                    this.board = copy;
+            Collection<ChessMove> copyMoves = new ArrayList<>(moves);
+            for (ChessMove move : copyMoves) {
+                board.addPiece(move.getEndPosition(), copy.getPiece(startPosition));
+                board.addPiece(startPosition, null);
+                if (isInCheck(getTeamTurn())) {
+                    moves.remove(move);
                 }
+                this.board = copy;
             }
         } catch (CloneNotSupportedException e){
             throw new RuntimeException("Cloning Error", e);
@@ -89,8 +90,8 @@ public class ChessGame {
 
         board.addPiece(move.getEndPosition(), board.getPiece(move.getStartPosition()));
         board.addPiece(move.getStartPosition(), null);
-        if (teamTurn == TeamColor.WHITE){ teamTurn = TeamColor.BLACK; }
-        else if (teamTurn == TeamColor.BLACK){ teamTurn = TeamColor.WHITE; }
+        if (teamTurn == TeamColor.WHITE){ setTeamTurn(TeamColor.BLACK); }
+        else if (teamTurn == TeamColor.BLACK){ setTeamTurn(TeamColor.WHITE); }
     }
 
     /**
@@ -102,14 +103,15 @@ public class ChessGame {
     public boolean isInCheck(TeamColor teamColor) {
         ChessPosition kingSpot = getKingSquare(teamColor);
         Collection<ChessMove> threats = new ArrayList<>();
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                ChessPiece piece = this.board.board[i][j];
-                if (piece != null && piece.getTeamColor() != teamColor){
-                    threats.addAll(piece.pieceMoves(board, new ChessPosition(i+1, j+1)));
-                }
-            }
+        TeamColor opponent;
+        if (teamColor == TeamColor.WHITE) { opponent = TeamColor.BLACK; }
+        else { opponent = TeamColor.WHITE; }
+        Collection<ChessPosition> opponentPositions = teamPositions(opponent);
+
+        for (ChessPosition position : opponentPositions) {
+            threats.addAll(board.getPiece(position).pieceMoves(board, position));
         }
+
         for (ChessMove move : threats){
             if (move.getEndPosition().equals(kingSpot)){
                 return true;
@@ -187,7 +189,6 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        board.resetBoard();
         this.board  = board;
     }
 
