ed6e009a641e6101c2f59f42a0da8f018d36e40e
Levi Draughon <ldraugho@byu.edu>
2026-02-23 21:44:44 -0700

    small UserDAO changes

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -6,4 +6,8 @@ public class UserDAO {
     public static UserData getUser(String username) {
         throw new RuntimeException("Not implemented");
     }
+    public void createUser(String username, String password, String email) {
+        throw new RuntimeException("Not implemented");
+    }
+
 }
