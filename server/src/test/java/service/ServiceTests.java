fab1f0cfc6a9ab28eb1d2c424342f74f13409078
Levi Draughon <ldraugho@byu.edu>
2026-03-02 21:25:27 -0700

    All tests completed

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -46,13 +46,11 @@ public class ServiceTests {
     @Test
     public void registerFailTest() throws DataAccessException {
         users.register(new RegisterRequest("levi", "1234", "email"));
-        assertThrows(DataAccessException.class, () -> {
-            users.register(new RegisterRequest("levi", "5678", "email"));;
-        });
+        assertThrows(DataAccessException.class, () -> users.register(new RegisterRequest("levi", "5678", "email")));
     }
 
     @Test
-    public void loginSuccessTest() throws DataAccessException, BadRequestException {
+    public void loginSuccessTest() throws DataAccessException {
         users.register(new RegisterRequest("levi", "1234", "email"));
         LoginResult result = users.login(new LoginRequest("levi", "1234"));
         assert (Objects.equals(result.username(), "levi"));
@@ -60,10 +58,8 @@ public class ServiceTests {
     }
 
     @Test
-    public void loginFailTest() throws DataAccessException {
-        assertThrows(UnauthorizedException.class, () -> {
-            users.login(new LoginRequest("levi", "1234"));
-        });
+    public void loginFailTest() {
+        assertThrows(UnauthorizedException.class, () -> users.login(new LoginRequest("levi", "1234")));
     }
 
     @Test
@@ -74,10 +70,90 @@ public class ServiceTests {
     }
 
     @Test
-    public void logoutFailTest() throws DataAccessException{
-        assertThrows(UnauthorizedException.class, () -> {
-            users.logout(new LogoutRequest(generateToken()));
-        });
+    public void logoutFailTest() {
+        assertThrows(UnauthorizedException.class, () -> users.logout(new LogoutRequest(generateToken())));
+    }
+
+    @Test
+    public void createGameSuccessTest() throws DataAccessException{
+        RegisterResult register =  users.register(new RegisterRequest("levi", "1234", "email"));
+        NewGameResult result = games.createGame(new NewGameRequest("test game"), register.authToken());
+
+        assert (gameDAO.getGame(result.gameID()) != null);
+    }
+
+    @Test
+    public void createGameFailTest() {
+        assertThrows(UnauthorizedException.class, () -> games.createGame(new NewGameRequest("name"), generateToken()));
+    }
+
+    @Test
+    public void joinGameSuccessTest() throws DataAccessException{
+        RegisterResult register =  users.register(new RegisterRequest("levi", "1234", "email"));
+        NewGameResult game = games.createGame(new NewGameRequest("test game"), register.authToken());
+        games.joinGame(new JoinRequest("WHITE", game.gameID()), register.authToken());
+
+        assert (Objects.equals(gameDAO.getGame(game.gameID()).whiteUsername(), "levi"));
+    }
+
+    @Test
+    public void joinGameFailTest() throws DataAccessException{
+        RegisterResult register =  users.register(new RegisterRequest("levi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+
+        assertThrows(BadRequestException.class, () -> games.joinGame(new JoinRequest("WHITE", 5), register.authToken()));
+    }
+
+    @Test
+    public void listGamesSuccessTest() throws DataAccessException{
+        RegisterResult register = users.register(new RegisterRequest("levi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("lvi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("lei", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("evi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+
+        assert (games.listGames(register.authToken()) != null);
+    }
+
+    @Test
+    public void listGamesFailTest() {
+        assertThrows(UnauthorizedException.class, () -> users.logout(new LogoutRequest(generateToken())));
+    }
+
+    @Test
+    public void clearSuccessTest() throws DataAccessException{
+        RegisterResult register = users.register(new RegisterRequest("levi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("lvi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("lei", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+        users.logout(new LogoutRequest(register.authToken()));
+        register = users.register(new RegisterRequest("evi", "1234", "email"));
+        games.createGame(new NewGameRequest("test game"), register.authToken());
+
+        users.clear();
+        games.clear();
+        auths.clear();
+
+        Assertions.assertThrows(BadRequestException.class, () -> gameDAO.getGame(1));
+        Assertions.assertNull(userDAO.getUser("levi"), "users not cleared");
+        Assertions.assertNull(authDAO.getAuth(register.authToken()), "auths not cleared");
+    }
+
+    @Test
+    public void clearNoneTest() {
+        users.clear();
+        auths.clear();
+        games.clear();
     }
 
 }
