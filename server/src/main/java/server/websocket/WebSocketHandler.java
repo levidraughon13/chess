816b3d352038b3f0c3b31f75f73b559dd4d22b65
Levi Draughon <ldraugho@byu.edu>
2026-04-06 10:55:02 -0600

    Create a few new classes for Phase 6, just trying to get some structure to help me understand it all.

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,56 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import io.javalin.websocket.*;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @Override
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
+    }
+
+    @Override
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            UserGameCommand action = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+
+            switch (action.getCommandType()) {
+                case CONNECT -> connect(action.visitorName(), ctx.session);
+                case LEAVE -> exit(action.visitorName(), ctx.session);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
+
+    private void connect(String visitorName, Session session) throws IOException {
+        connections.add(session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(session, notification);
+    }
+
+
+    private void exit(String visitorName, Session session) throws IOException {
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(session, notification);
+        connections.remove(session);
+    }
+
+}
